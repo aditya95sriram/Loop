081d9363ea5ba85c4fa1e35f9c11963c2d72ce19
aditya95sriram <aditya95sriram@gmail.com>
2013-09-26 10:37:36 +0530

    Aded 'pretty' function to print current puzzle state using special ASCII characters. Also added HTML helper to easily input new puzzles.

---
+++ loop.js
@@ -0,0 +1,123 @@
+$(document).ready(function() {
+    // jQery object variables
+    var $board = $('#board'),
+        $dot   = '<div class="element dot"></div>';
+                
+    // Variables for holding dimensions of 'divs'
+    var m = 5, d1 = 10, d2 = 50; 
+    var bdim = (7*(m + d1 + m + d2) + m + d1 + m);
+    
+    // Varaibles for holding data about numbers and filled lines
+    var numArray = Array(7);
+    for (var i=0; i<7; i++) { numArray[i] = [-1, -1, -1, -1, -1, -1, -1]; }    
+    
+    // pastebin api variables
+    var key          = '4729cb33284750f5bd3714d120641b7e',
+        paste_option = 'paste',
+        paste_status = '1',
+        paste_expire = '10M',
+        paste_format = 'python',
+        api_url      = 'http://pastebin.com/api/api_post.php';
+        
+    var line = function(dir, y, x) {
+        s = '<div class="element {0}-line line" id="{1}-{2}"></div>';
+        return s.replace("{0}", dir).replace("{1}", y).replace("{2}", x); 
+    }
+    
+    var entry = function(y, x) {
+        s = '<div class="element entry" id="{0}-{1}"></div>';
+        return s.replace("{0}", y).replace("{1}", x);
+    }
+    
+    var printH = function(y) {
+        for (var i=0; i<7; i++) {
+            $board.append($dot, line('h', y, 2*i+1));
+        }
+        $board.append($dot);
+    }
+    
+    var printV = function(y) {
+        for (var i=0; i<7; i++) {
+            $board.append(line('v', y, 2*i), entry((y-1)/2, i));
+        }
+        $board.append(line('v', y, 2*7));
+    }
+    
+    var init = function() {
+        
+        $board.css({width: bdim, height: bdim});
+        //$board.css("backgroundColor", "pink");
+        for (var j=0; j<7; j++) {
+            printH(2*j);
+            printV(2*j + 1);
+        }
+        printH(2*7);
+    }
+    
+    init();
+    
+    $('.line').click(function() {
+        $(this).toggleClass("dark");
+    });
+    
+    $('#panel').mouseenter(function() {
+        $(this).animate({borderRadius: '+=20px'}, 200);
+    });
+    $('#panel').mouseleave(function() {
+        $(this).animate({borderRadius: '-=20px'}, 400);
+    });
+        
+    $('.entry').click(function() {
+        var s = (this.id).split("-");
+        var n = prompt("Number");
+        $(this).text(n);
+        numArray[parseInt(s[0])][parseInt(s[1])] = parseInt(n);
+        console.log(parseInt(s[0]), parseInt(s[1]));
+        console.log(numArray);
+    });
+    
+    $('#panel').click(function() {
+    
+        s = "[["
+        s += numArray.join("],<br>&nbsp;[")
+        s += "]]"
+        $('#text p').html(s);
+        
+        /*
+        $.ajax({type    : "POST",
+                url     : api_url,
+                isLocal : true,
+                data    : { api_dev_key      : key,
+                            api_option       : paste_option,
+                            api_paste_private: paste_status,
+                            api_paste_expire : paste_expire,
+                            api_paste_format : paste_format,
+                            api_paste_code   : encodeURIComponent(numArray) },
+                dataType: "text",
+                success : function(data) {
+                            alert("success");
+                            console.log("Success\n" + data);
+                          }, 
+                error   : function(jqXHR, textStatus, errorThrown) {
+                            alert(textStatus);
+                            console.log("Error\n" + errorThrown);
+                            console.log("jqXHR");
+                            console.log(jqXHR);
+                          },
+                complete: function(jqXHR, textStatus) {
+                            alert(textStatus);
+                            console.log("Complete jqXHR");
+                            console.log(jqXHR);
+                          }
+                .done(function(data) 
+                {
+                    prompt("success");
+                    console.log("Success\n" + data);
+                }
+                  });
+                  
+        */
+    });
+    
+    
+});
--- loop.py
+++ loop.py
@@ -1,10 +1,4 @@
-# Constants
-DOT = '9'
-NOLINE = '8'
-LINE = '7'
-CROSS = '6'
-NUMBER = '5'
-constants = dict(zip([DOT,NOLINE,CROSS], ['.',' ','x']))
+import os
 
 def log(m):
     global DEBUG_MODE
@@ -34,7 +28,7 @@ class array(list):
         row[key[1]] = item
         list.__setitem__(self, key[0], row)
 
-class lineArray(object):
+class Board(object):
     """Class for maintaining state of individual lines in the
     puzzle. Uses array of larger dimension to accomodate both
     horizontal and vertical lines in a single array.
@@ -49,46 +43,129 @@ class lineArray(object):
     Any other number represents numeral in the puzzle
     """
     order = 7
+
+    # Constants
+    DOT = '9'
+    NOLINE = '8'
+    LINE = '7'
+    CROSS = '6'
+    NUMBER = '5'
+    constants = dict(zip([DOT,NOLINE,CROSS], ['.',' ','x']))
+
+    # ASCII art lines
+    VERTICAL     = chr(186)
+    HORIZONTAL   = chr(205)
+    connectors = {"LEFT_BOTTOM" : chr(187),
+                  "LEFT_TOP"    : chr(188),
+                  "RIGHT_TOP"   : chr(200),
+                  "RIGHT_BOTTOM": chr(201)}
+
     def __init__(self):
-        order, tempArray = lineArray.order, []
+        order, tempArray = Board.order, []
         for i in range(2*order + 1):
             if i % 2 == 0:  # horizontal lines row
                 tempLine = []
                 for n in range(2*order + 1):
-                    tempLine.append((NOLINE if n%2 else DOT))
+                    tempLine.append((Board.NOLINE if n%2 else Board.DOT))
                 tempArray.append(tempLine)
             else:           # vertical lines row
                 tempLine = []
                 for n in range(2*order + 1):
-                    tempLine.append(NUMBER if n%2 else NOLINE)
+                    tempLine.append(Board.NUMBER if n%2 else Board.NOLINE)
                 tempArray.append(tempLine)
         self.lines = array(tempArray)
 
-    def __str__(self):
-        s = ""
+    def display(self, outArray = False):
+        if outArray:  # output required in array format
+            a = []
+        else:         # output required in string format
+            s = ""
+        seperator = "" if outArray else " "
         for row in self.lines:
-            line = " ".join([constants.get(i,i) for i in row])
-            if row[0] == DOT:    # horizontal lines row
-                line = line.replace(LINE, '_')
+            line = seperator.join([Board.constants.get(i,i) for i in row])
+            if row[0] == Board.DOT:    # horizontal lines row
+                #line = line.replace(LINE, '_')
+                line = line.replace(Board.LINE, Board.HORIZONTAL)
             else:              # vertical lines row
-                line = line.replace(LINE, '|')
-            s += line + '\n'
-        return s
-    
+                #line = line.replace(LINE, '|')
+                line = line.replace(Board.LINE, Board.VERTICAL)
+            if outArray: # output required in array format
+                a.append(list(line))
+            else:        # output required in string format
+                s += line + '\n'
+        return array(a) if outArray else s
+
+    def __str__(self):
+        return self.display(False)
+
+    def isValid(self, y, x):
+        ymax = xmax = 2*Board.order
+        return all((y>=0, x>=0, y<=ymax, x<=xmax))
+
+    def pos(self, y, x, optimal = False):
+        """
+        Diagram for reference in comments
+
+         x (a) x (b) x
+        (c) . [ ]   (d)
+         x (e) x (f) x
+
+        Square brackets show current cell
+        Round brackets show cells to check
+
+        'optimal' mode returns only the cell to the left in
+        the same row to avoid redundancy while processing all
+        the cells in left to right and top to bottom fashion.
+        """
+        p = [(y-1,x-1), (y-1,x+1),  # (a), (b)
+             (y  ,x-2), (y,  x+2),  # (c), (d)
+             (y+1,x-1), (y+1,x+1)]  # (e), (f)
+        if optimal: p.remove((y, x+2))
+        return [(a,b) for a,b in p if self.isValid(a, b)]
+
+    def pretty(self):
+        lines = self.display(outArray = True)  # array format output
+        j = Board.connectors
+        for y, row in enumerate(lines):
+            if row[0] == Board.constants[Board.DOT]: # horizontal lines row
+                for x, cell in enumerate(row):
+                    if cell == Board.HORIZONTAL:
+                        p = self.pos(y, x, True)
+                        connected = 0
+                        for c in p:
+                            if lines[c] == Board.HORIZONTAL:
+                                lines[c[0], (c[1]+x)/2] = Board.HORIZONTAL
+                                connected += 1
+                            elif lines[c] == Board.VERTICAL:
+                                place = ["RIGHT_","LEFT_"][int(c[1] > x)]
+                                place += ["TOP","BOTTOM"][int(c[0] > y)]
+                                lines[y, c[1]] = j[place]
+                                connected += 1
+                            if connected == 2:
+                                break
+            else: # vertical lines row
+                for x, cell in enumerate(row):
+                    if cell == Board.VERTICAL:
+                        if y > 2 and lines[y-2, x] == Board.VERTICAL:
+                            lines[y-1, x] = Board.VERTICAL
+        s = "\n".join([" ".join(line) for line in lines])
+        h = Board.HORIZONTAL
+        s = s.replace(' ' + h + ' ', h*3) # " _ " => '___'
+        print s
+
     def inject(self, numArray):
+        self.numArray = numArray
         for y, row in enumerate(numArray):
             for x, cell in enumerate(row):
                 if cell != -1:
                     self.lines[2*y + 1, 2*x + 1] = str(cell)
                 else:
                     self.lines[2*y + 1, 2*x + 1] = ' '
-                
-class loopBoard(object):
 
-    order = 7
-
-    def __init__(self, numArray):
-        self.numArray = array(numArray)
+    def edit(self):
+        os.startfile(r"H:\My DOcs\Portable Python\Scripts\GUI\Loop\loop.html")
+        a = eval(raw_input("Array: \n"))
+        if a: self.numArray = a
 
 puzzle1 = [(0,1),(0,3),(0,7),(0,13),
            (1,0),(1,4),(1,6),(1,8),(1,12),(1,14),
@@ -102,7 +179,7 @@ puzzle1 = [(0,1),(0,3),(0,7),(0,13),
            (9,8),(9,10),
            (10,1),(10,3),(10,5),(10,7),(10,11),(10,13),
            (11,0),(11,14),
-           (12,3),(12,7),(12,9),
+           (12,3),(12,7),(12,11),
            (13,0),(13,2),(13,4),(13,6),(13,8),(13,10),(13,12),(13,14),
            (14,1),(14,5),(14,9),(14,13)]
 
@@ -114,14 +191,31 @@ num1 = array([[ 3, 2, 3, 3, 2, 1, 3],
               [-1, 2,-1,-1, 0, 2, 2],
               [-1, 3,-1, 3, 3, 3,-1]])
 
+num2 = array([[-1,-1,3,3,3,2,3],
+              [3,1,2,0,-1,-1,3],
+              [1,2,2,-1,-1,2,2],
+              [-1,-1,-1,-1,2,-1,-1],
+              [-1,-1,1,-1,3,2,2],
+              [2,-1,2,-1,-1,-1,2],
+              [-1,-1,3,2,3,2,2]])
+
+num3 = array([[3,-1,-1,3,1,2,-1],
+              [-1,-1,-1,1,-1,-1,-1],
+              [2,-1,-1,2,2,1,-1],
+              [-1,3,0,2,2,-1,-1],
+              [2,-1,2,-1,-1,-1,-1],
+              [3,-1,-1,-1,2,-1,-1],
+              [-1,2,1,2,-1,-1,-1]])
+
 if __name__ == "__main__":
     DEBUG_MODE = False
-    l = lineArray()
-    a = l.lines
-    for p in puzzle1: a[p] = LINE
-    print
-    l.inject(num1)
-    print '\n', l, '\n'
+    b = Board()
+    #for p in puzzle1: a[p] = Board.LINE
+    b.inject(num3)
+    #print l
+    b.pretty()
+
+
 
 
 
